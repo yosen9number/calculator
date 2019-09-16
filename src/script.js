189404fea9d6e98d539793e1c39312ffd252a922
Наира Лалаян <nlalayan@ivi.ru>
2019-09-16 03:29:58 +0300

    попытки

--- src/script.js
+++ src/script.js
@@ -22,25 +22,35 @@
     function calculate(operands, operators) {
         let result = Number(operands[0]);
         let operator;
-        let firstOperand = Number(operands[0]);
+        let firstOperatorsResult = 0;
+
+        let arrOperators = operators.split("");
+        console.log(arrOperators);
+
+        let arrOperands = operands.split("");
+        console.log(arrOperands);
 
 
-        let a = operators.split("");
-        console.log(a);
-
-
-        for (let i = 0; i < operators.length; i++) {
-            operator = operators[i];
+        for (let i = 0; i < arrOperators.length; i++) {
+            operator = arrOperators[i];
             if (operator === "*") {
-                result = result * Number(operands[i+1]);
+                firstOperatorsResult = Number(operands[i]) * Number(operands[i+1]);
+                new arrOperators.splice(i, 1);
+                new arrOperands.splice(i, 2, firstOperatorsResult);
             } else if (operator === "/") {
-                result = result / Number(operands[i+1]);
+                firstOperatorsResult = Number(operands[i]) / Number(operands[i+1]);
+                new arrOperators.splice(i, 1);
+                new arrOperands.splice(i, 2, firstOperatorsResult);
             }
-
-            /*else if ((operator === "+") || (operator === "-")) {
-                result = (operator === "+") ? result + Number(numbers[i+1]) : result - Number(numbers[i+1]);
-            }*/
-            console.log(operator, result);
+        }
+        for (let j = 0; j < arrOperators.length; j++) {
+            operator = arrOperators[j];
+            if (operator === "+") {
+                result = result + Number(operands[j+1]);
+            } else if (operator === "-") {
+                result = result - Number(operands[j+1]);
+            }
+            console.log(arrOperators, arrOperands, result);
         }
         return result;
     }
