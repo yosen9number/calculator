8e63db0c8220ad22755a5a4b850ac9f90eb4f2a4
Наира Лалаян <nlalayan@ivi.ru>
2019-10-07 14:59:02 +0300

    ещё изменения

--- src/script.js
+++ src/script.js
@@ -1,10 +1,8 @@
 (function () {
-    const inputValue = document.getElementById("calc_input") ;
     const calcValue = document.getElementById("calc_value") ;
-    const pattern = /([0-9]+)([\+|\-|\*|\/]+)/;
 
     form.addEventListener("change", function(event) {
-        var target = event.target;
+        const target = event.target;
         let stack = [];
 
         target.value.split(' ').forEach((token) => {
@@ -21,48 +19,11 @@
     });
 
     const operators = {
-        '+': (x, y) => x + y,
-        '-': (x, y) => x - y,
-        '*': (x, y) => x * y,
-        '/': (x, y) => x / y
+        '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+        '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+        '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+        '/': (firstOperand, secondOperand) => firstOperand / secondOperand
     };
-
-
-    /*function calculate(operands, operators) {
-        let result = Number(operands[0]);
-        let operator;
-        let firstOperatorsResult = 0;
-
-        let arrOperators = operators.split("");
-        console.log(arrOperators);
-
-        let arrOperands = operands.split("");
-        console.log(arrOperands);
-
-
-        for (let i = 0; i < arrOperators.length; i++) {
-            operator = arrOperators[i];
-            if (operator === "*") {
-                firstOperatorsResult = Number(operands[i]) * Number(operands[i+1]);
-                new arrOperators.splice(i, 1);
-                new arrOperands.splice(i, 2, firstOperatorsResult);
-            } else if (operator === "/") {
-                firstOperatorsResult = Number(operands[i]) / Number(operands[i+1]);
-                new arrOperators.splice(i, 1);
-                new arrOperands.splice(i, 2, firstOperatorsResult);
-            }
-        }
-        for (let j = 0; j < arrOperators.length; j++) {
-            operator = arrOperators[j];
-            if (operator === "+") {
-                result = result + Number(operands[j+1]);
-            } else if (operator === "-") {
-                result = result - Number(operands[j+1]);
-            }
-            console.log(arrOperators, arrOperands, result);
-        }
-        return result;
-    }*/
 })();
 
 
