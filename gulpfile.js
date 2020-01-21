99b78063dffa081c6cb1cbd4b97e43cbb872b8ba
Наира Лалаян <nlalayan@ivi.ru>
2020-01-22 01:49:22 +0300

    промежуточный результат добавления bundle

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp");
+const browserify = require("browserify");
+const source = require('vinyl-source-stream');
+const tsify = require("tsify");
+const paths = {
+    pages: ['/*.html']
+};
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["copy-html"], function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"));
+});
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+function hello(compiler: string) {
+    console.log(`Привет от ${compiler}`);
+}
+hello("TypeScript");
